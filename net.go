3310d5a2012730a0210e9dacbd4e71a3b2e3da1a
Benjamin Cooley <benjamincooley81@gmail.com>
2019-07-09 10:56:12 -0700

    Update net.go

--- net.go
+++ net.go
@@ -1,1 +1,78 @@
-package net
+package gonet
+
+import (
+	"bufio"
+	"net"
+)
+
+// Server ...
+type Server struct {
+	ln   net.Listener
+	conn net.Conn
+}
+
+// Start ...
+func (s *Server) Start(port string) (err error) {
+	s.ln, err = net.Listen("tcp", port)
+	if err != nil {
+		return err
+	}
+	s.conn, err = s.ln.Accept()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// OnMessage ...
+func (s Server) OnMessage(on func(msg string)) (err error) {
+	for {
+		message, err := bufio.NewReader(s.conn).ReadString('\n')
+		if err != nil {
+			return err
+		}
+		go on(message)
+	}
+}
+
+// Send ...
+func (s Server) Send(msg string) {
+	s.conn.Write([]byte(msg + "\n"))
+}
+
+// Client ...
+type Client struct {
+	ip   string
+	conn net.Conn
+}
+
+// Connect ...
+func (c *Client) Connect(ip string) (err error) {
+	c.ip = ip
+	c.conn, err = net.Dial("tcp", ip)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// OnMessage ...
+func (c Client) OnMessage(on func(msg string)) (err error) {
+	for {
+		message, err := bufio.NewReader(c.conn).ReadString('\n')
+		if err != nil {
+			return err
+		}
+		go on(message)
+	}
+}
+
+// Send ...
+func (c Client) Send(msg string) {
+	c.conn.Write([]byte(msg + "\n"))
+}
+
+// Disconnect ...
+func (c Client) Disconnect() {
+	c.conn.Close()
+}
